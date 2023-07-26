c430a7ebd81891103e1bf30c070f7dfe7c245805
Yasin Asadnezhad <yasin.asadnezhad@msn.com>
2023-07-26 22:42:12 +0330

    Configured express server.

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,38 @@
 const express = require("express");
+const PORT = 8080;
 const app = express(); // create express app
+
+//Read data from json
+const data = require('./api/api');
+ 
+app.use(express.json());
+
+app.use(function (req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*");
+
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-credentials", 'true');
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  next();
+});
+
 
 app.get("/", (req, res) => {
   res.send("This is from express.js");
 });
 
-// start express server on port 5000
-app.listen(5000, () => {
-  console.log("server started on port 5000");
+
+//Run server using #Yarn Server
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
 });
+
+/**
+ * service method to retrieve test data
+ * @returns hello
+ */
+app.get("/api/hello", (req, res) => {
+  res.send({ message: "Hello" });
+});
+  
